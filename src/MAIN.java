5e5bddab983ec7a6e35fb4a7f472f30e804f5fea
Moritz-10b <moritzshield@gmail.com>
2022-03-17 22:21:15 +0100

    Changes by Severin

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,6 +1,5 @@
 public class MAIN {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("des projekt wird geil");
+        new GAME();
     }
 }
