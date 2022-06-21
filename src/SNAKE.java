7d0493d7a86926f363e1e503196760f217bd8c34
Moritz <moritzshield@gmail.com>
2022-06-21 09:38:25 +0200

    snake brattle

--- src/SNAKE.java
+++ src/SNAKE.java
@@ -8,4 +8,36 @@ public class SNAKE {
     public void ChangeDir(int dir) {
         this.dir = dir;
     }
+    public void move() {
+        switch (dir) {
+            case 0 -> {
+                if (y > 0) {
+                    y--;
+                } else {
+                    y = 35;
+                }
+            }
+            case 1 -> {
+                if (x < 63) {
+                    x++;
+                } else {
+                    x = 0;
+                }
+            }
+            case 2 -> {
+                if (y < 35) {
+                    y++;
+                } else {
+                    y = 0;
+                }
+            }
+            case 3 -> {
+                if (x > 0) {
+                    x--;
+                } else {
+                    x = 63;
+                }
+            }
+        }
+    }
 }
