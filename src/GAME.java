134eee29baf366cde8acaff0f1eb5832e29c8d53
Moritz-10b <moritzshield@gmail.com>
2022-07-02 20:06:06 +0200

    berechnung winkelrotation test

--- src/GAME.java
+++ src/GAME.java
@@ -16,6 +16,17 @@ public class GAME {
         //AUDIOPLAYER.Start();
         //playerList.add(new PLAYER("Severin", Color.black, 3, 11));
         //playerList.add(new PLAYER("Moritz", Color.orange, 3, 12));
+        int x = -10;
+        int y = -10;
+        double vx = 0;
+        double vy = -2;
+        double phi = Math.acos((-vx*x+vy*y)/((Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)))*(Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2)))));
+        double xneu = Math.round(vx*Math.cos(Math.PI - 2*phi) - vy*Math.sin(Math.PI - 2*phi));
+        double yneu = Math.round(vx*Math.sin(Math.PI - 2*phi) + vy*Math.cos(Math.PI - 2*phi));
+        System.out.println(phi + "   " + Math.toDegrees(phi));
+        System.out.println("x: " + xneu);
+        System.out.println("y: " + yneu);
+
     }
     public static void NextPlayer() {
         if(currentPlayer < playerList.size() - 1) {
